74da63a7701d6bde0cb183c90849f5354da926d3
Harshvardhan1809 <harrygarry2002@gmail.com>
2025-07-07 15:27:06 +0900

    gyroscope working

--- src/main.rs
+++ src/main.rs
@@ -1,159 +1,72 @@
-// use mpu6050::*;
-// use linux_embedded_hal::{I2cdev, Delay};
-// use i2cdev::linux::LinuxI2CError;
-// // use embedded_hal::blocking::i2c::{WriteRead, Write};
-// // use embedded_hal::blocking::delay::DelayMs;
-// use shared_bus;
-// use i2cdev::linux::{LinuxI2CDevice, LinuxI2CBus};
-// use embedded_hal_bus::i2c;
-// use embedded_hal_bus::util::AtomicCell;
-// // use std::ops::{Deref, DerefMut};
-
-// struct MyI2cdev(I2cdev);
-
-// // https://docs.esp-rs.org/esp-idf-hal/src/esp_idf_hal/i2c.rs.html#202-205
-
-// // impl WriteRead for MyI2cdev {
-// //     type Error = LinuxI2CError;
-
-// //     fn write_read(&mut self, addr: u8, bytes: &[u8], buffer: &mut [u8]) -> Result<(), Self::Error> {
-// //         MyI2cdev::write_read(self, addr, bytes, buffer).map_err(to_i2c_err)
-// //     }
-// // }
-
-// // impl Deref for MyI2cdev {
-// //     type Target = I2cdev;
-// //     fn deref(&self) -> &I2cdev {
-// //         &self.0
-// //     }
-// // }
-
-// // impl DerefMut for MyI2cdev {
-// //     fn deref_mut(&mut self) -> &mut I2cdev {
-// //         &mut self.0
-// //     }
-// // }
-
-// // impl Write for MyI2cdev {
-// //     type Error = LinuxI2CError;
-
-// //     fn write(&mut self, addr: u8, bytes: &[u8]) -> Result<(), Self::Error> {
-// //         MyI2cdev::write(self, addr, bytes).map_err(to_i2c_err)
-// //     }
-// // }
-
-// // // Specific implementations of the Error can be done later
-// // fn to_i2c_err(err: LinuxI2CError) -> LinuxI2CError {
-// //     err
-// // }
-
-
-// fn main() -> Result<(), Mpu6050Error<LinuxI2CError>> {
-
-//     // make linux driver, then shared bus, then mpu
-
-//     // we can possibly define multiple i2cdev for multiple sensors since i2cdev is for a device
-//     let i2c = I2cdev::new("/dev/i2c-1").map_err(Mpu6050Error::I2c).expect("");
-
-//     let mpu = LinuxI2CDevice::new("/dev/i2c-1", 0x68);
-//     // let mpu = LinuxI2CBus::new("/dev/i2c-1");
-
-
-//     // println!("1\n");
-
-//     // let mut delay = Delay;
-
-//     // let i2c_bus = AtomicCell::new(i2c);
-//     // let i2c_bus = shared_bus::BusManagerSimple::new(i2c);
-//     // let i2c_new = i2c::AtomicDevice::new(&i2c_bus);
-//     // try both 68 and 69
-//     let mut mpu = Mpu6050::new(i2c);
-
-
-//     println!("4\n");
-
-//     loop {
-//         // get roll and pitch estimate
-//         let acc = mpu.get_acc_angles().expect("");
-//         println!("r/p: {:?}", acc);
-
-//         // get sensor temp
-//         let temp = mpu.get_temp().expect("");
-//         println!("temp: {:?}c", temp);
-
-//         // get gyro data, scaled with sensitivity
-//         let gyro = mpu.get_gyro().expect("");
-//         println!("gyro: {:?}", gyro);
-
-//         // get accelerometer data, scaled with sensitivity
-//         let acc = mpu.get_acc().expect("");
-//         println!("acc: {:?}", acc);
-//     }
-
-//     Ok(())
-// }
-
-// fn main() {
-//     println!("Hello World");
-// }
-
-// mod sensors;
-
-// use crate::sensors::mpu6050::{lib::Mpu6050Error, lib::Mpu6050};
-
-use mpu6050::device::{WHOAMI, DEFAULT_SLAVE_ADDR, PWR_MGMT_1};
-use linux_embedded_hal::{I2cdev, Delay};
-use embedded_hal::{delay::DelayNs, i2c::Error as I2CError};
-use i2cdev::linux::{LinuxI2CError, LinuxI2CDevice};
-use i2cdev::core::I2CDevice;
 use std::thread;
 use std::time::Duration;
+use std::rc::Rc;
+use std::cell::RefCell;
+use linux_embedded_hal::gpio_cdev::Chip;
+use i2cdev::linux::{LinuxI2CError, LinuxI2CDevice};
+use i2cdev::core::I2CDevice;
 
-fn main() -> Result<(), LinuxI2CError> {
+pub mod mpu6050;
+use mpu6050::utils::*;
+use mpu6050::device::{AccelRange, ACCEL_CONFIG, GyroRange, GYRO_CONFIG, ACCEL_HPF, INT_STATUS, WHOAMI, PWR_MGMT_1, MOT_DETECT_STATUS, ACCEL_SENS, GYRO_SENS};
 
-    println!("A");
-    let mut i2c = LinuxI2CDevice::new("/dev/i2c-1", 0x68)?;
+fn main() -> Result<(), (linux_embedded_hal::gpio_cdev::Error, LinuxI2CError)> {
 
-    println!("B");
+    // Let's start by checking the status of the I2C pins
+    // and potentially opening them
+    // If the line is held by the kernel,
+    // it needs to be freed
 
-    i2c.smbus_write_byte_data(PWR_MGMT_1::ADDR, 0x01).unwrap();
-    thread::sleep(Duration::from_millis(100));
+    println!("Check the status of PIN7 (I2C1_SDA)");
+    let mut chip = Chip::new("/dev/gpiochip4").expect("Failed to open GPIO chip");
+    let line = chip.get_line(20).expect("Got the line");
+    println!("PIN11 (I2C1_SDA) is held by the kernel {:?}", line.info().expect("").is_kernel());
+    println!("PIN11 (I2C_SDA) is held at low {:?}\n", line.info().expect("").is_active_low());
 
-    println!("C");
+    println!("Check the status of PIN6 (I2C1_SCL)");
+    let mut chip = Chip::new("/dev/gpiochip4").expect("Failed to open GPIO chip");
+    let line = chip.get_line(19).expect("Got the line");
+    println!("PIN12 (I2C1_SCL) is held by the kernel {:?}", line.info().expect("").is_kernel());
+    println!("PIN12 (I2C1_SCL) is held at low {:?}\n", line.info().expect("").is_active_low());
 
+    // Now we instantiate the sensor,
+    // wake it up, initialize its registers
+    // and take readings
 
-    println!("D");
+    println!("Instantiate device");
+    let i2c: Rc<RefCell<LinuxI2CDevice>> = Rc::new(RefCell::new(LinuxI2CDevice::new("/dev/i2c-1", 0x68).expect("")));
+    let mpu = Mpu6050::new(i2c.clone());
 
-    let address = i2c.smbus_read_byte_data(WHOAMI).unwrap();
+    println!("Wake up the device");
+    let _ = mpu.wake();
 
-    println!("E");
+    println!("Read the slave address");
+    let _ = mpu.read_slave_addr();
 
-    println!("MPU6050 Address: {:}", address);
+    println!("Set ranges");
+    mpu.init();
 
+    println!("Take readings");
+    loop {
+        // get roll and pitch estimate
+        let acc = mpu.get_acc_angles().expect("");
+        println!("r/p: {:?}", acc);
 
-    // let i2c = I2cdev::new("/dev/i2c-1").unwrap();
+        // get sensor temp
+        let temp = mpu.get_temp().expect("");
+        println!("temp: {:?}c", temp);
 
-    // let mut delay = Delay;
-    // let mut mpu = Mpu6050::new_with_addr(i2c, 0x68);
-    
-    // mpu.init(&mut delay).unwrap();
-    // mpu.setup_motion_detection().unwrap();
+        // get gyro data, scaled with sensitivity
+        let gyro = mpu.get_gyro().expect("");
+        println!("gyro: {:?}", gyro);
 
-    // let mut count: u8 = 0;
+        // // get accelerometer data, scaled with sensitivity
+        let acc = mpu.get_acc().expect("");
+        println!("acc: {:?}", acc);
 
-    // loop {
-    //     if mpu.get_motion_detected().unwrap() {
-    //         println!("YEAH BUDDY. Motion by axes: {:b}", mpu.read_byte(MOT_DETECT_STATUS::ADDR).unwrap());
-    //         count += 1;
-    //     }
+        println!("\n");
 
-    //     delay.delay_ms(10u32);
-
-    //     if count > 5 {
-    //         mpu.reset_device(&mut delay).unwrap();
-    //         break;
-    //     }
-    // }
-
+        thread::sleep(Duration::from_millis(1000));
+    }
     Ok(())
 }
---
+++ src/mpu6050/utils.rs
@@ -0,0 +1,167 @@
+use std::thread;
+use std::time::Duration;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use nalgebra::{Vector3, Vector2};
+use libm::{powf, atan2f, sqrtf};
+
+use i2cdev::linux::LinuxI2CDevice;
+use i2cdev::core::I2CDevice;
+
+use crate::mpu6050::device::{AccelRange, ACCEL_CONFIG, GyroRange, GYRO_CONFIG, ACCEL_HPF, INT_STATUS, WHOAMI, PWR_MGMT_1, ACCEL_SENS, GYRO_SENS, ACC_REGX_H, TEMP_OFFSET, TEMP_SENSITIVITY, TEMP_OUT_H, GYRO_REGX_H};
+use crate::mpu6050::bits;
+
+pub const PI: f32 = core::f32::consts::PI;
+pub const PI_180: f32 = PI / 180.0;
+
+pub struct Mpu6050 {
+    i2c: Rc<RefCell<LinuxI2CDevice>>,
+    acc_sensitivity: f32,
+    gyro_sensitivity: f32,
+}
+
+impl Mpu6050 {
+
+    pub fn new(i2c: Rc<RefCell<LinuxI2CDevice>>) -> Self {
+        Mpu6050 {
+            i2c: i2c.clone(),
+            acc_sensitivity: ACCEL_SENS.0,
+            gyro_sensitivity: GYRO_SENS.0,
+        }
+    }
+
+    pub fn wake(&self) -> Result<(), ()> {
+        self.i2c.borrow_mut().smbus_write_byte_data(PWR_MGMT_1::ADDR, 0x01).unwrap();
+        thread::sleep(Duration::from_millis(100));
+        Ok(())
+    }
+
+    pub fn read_slave_addr(&self) -> Result<(), ()> {
+        let address = self.i2c.borrow_mut().smbus_read_byte_data(WHOAMI).unwrap();
+        thread::sleep(Duration::from_millis(100));
+        println!("Address : {}", address);
+        if address != 0x68 {
+            // panic
+            println!("Wrong slave address");
+            return Ok(());
+        }
+        Ok(())
+    }
+
+    pub fn init(&self) {
+        self.set_accel_range(AccelRange::G2);
+        self.set_gyro_range(GyroRange::D250);
+        self.set_accel_hpf(ACCEL_HPF::_RESET);
+    }
+
+    pub fn write_read(&self, address: u8, write: &[u8], read: &mut [u8]) -> Result<(), ()> {
+
+        self.i2c.borrow_mut().write(write).unwrap();
+        Ok(self.i2c.borrow_mut().read(read).unwrap())
+    }
+
+    pub fn read_bytes(&self, slave_addr: u8, reg: u8, buf: &mut [u8]) -> Result<(), ()> {
+        match self.write_read(slave_addr, &[reg], buf) {
+            Ok(a) => Ok(a),
+            Err(_) => Err(())
+        }
+    }
+
+    pub fn write_bits(&self, reg: u8, start_bit: u8, length: u8, data: u8) {
+        let mut byte: [u8; 1] = [0; 1];
+        self.read_bytes(0x68, reg, &mut byte).unwrap();
+        bits::set_bits(&mut byte[0], start_bit, length, data);
+        self.i2c.borrow_mut().smbus_write_byte_data(reg, byte[0]).unwrap();
+    }
+
+    // pub fn read_bit(i2c: Rc<RefCell<LinuxI2CDevice>>, reg: u8, bit_n: u8) -> Result<u8, ()> {
+    //     let mut byte: [u8; 1] = [0; 1];
+    //     read_bytes(i2c.clone(), 0x68, reg, &mut byte).unwrap();
+    //     println!("The byte is: {}", byte[0]);
+    //     Ok(bits::get_bit(byte[0], bit_n))
+    // }
+
+    pub fn set_accel_range(&self, range: AccelRange) -> Result<(), ()> {
+        self.write_bits(ACCEL_CONFIG::ADDR, ACCEL_CONFIG::FS_SEL.bit, ACCEL_CONFIG::FS_SEL.length, range as u8);
+        // acc_sensitivity = range.sensitivity();
+        Ok(())
+    }
+
+    pub fn set_gyro_range(&self, range: GyroRange) -> Result<(), ()> {
+        self.write_bits(GYRO_CONFIG::ADDR, GYRO_CONFIG::FS_SEL.bit, GYRO_CONFIG::FS_SEL.length, range as u8);
+        // gyro_sensitivity = range.sensitivity();
+        Ok(())
+    }
+
+    pub fn set_accel_hpf(&self, mode: ACCEL_HPF) -> Result<(), ()> {
+        Ok(self.write_bits(ACCEL_CONFIG::ADDR, ACCEL_CONFIG::ACCEL_HPF.bit, ACCEL_CONFIG::ACCEL_HPF.length, mode as u8))
+    }
+
+    // pub fn get_motion_detected(i2c: Rc<RefCell<LinuxI2CDevice>>) -> Result<bool, ()> {
+    //     Ok(read_bit(i2c.clone(), INT_STATUS::ADDR, INT_STATUS::MOT_INT)? != 0)
+    // }
+
+    fn read_word_2c(&self, byte: &[u8]) -> i32 {
+        let high: i32 = byte[0] as i32;
+        let low: i32 = byte[1] as i32;
+        let mut word: i32 = (high << 8) + low;
+
+        if word >= 0x8000 {
+            word = -((65535 - word) + 1);
+        }
+
+        word
+    }
+
+    fn read_rot(&self, reg: u8) -> Result<Vector3<f32>, ()> {
+        let mut buf: [u8; 6] = [0; 6];
+        self.read_bytes(0x68, reg, &mut buf).unwrap();
+
+        Ok(Vector3::<f32>::new(
+            self.read_word_2c(&buf[0..2]) as f32,
+            self.read_word_2c(&buf[2..4]) as f32,
+            self.read_word_2c(&buf[4..6]) as f32
+        ))
+    }
+
+    pub fn get_acc(&self) -> Result<Vector3<f32>, ()> {
+        let mut acc = self.read_rot(ACC_REGX_H)?;
+        acc /= self.acc_sensitivity;
+
+        Ok(acc)
+    }
+
+    pub fn get_acc_angles(&self) -> Result<Vector2<f32>, ()> {
+        let acc = self.get_acc()?;
+
+        Ok(Vector2::<f32>::new(
+            atan2f(acc.y, sqrtf(powf(acc.x, 2.) + powf(acc.z, 2.))),
+            atan2f(-acc.x, sqrtf(powf(acc.y, 2.) + powf(acc.z, 2.)))
+        ))
+    }
+
+    pub fn get_temp(&self) -> Result<f32, ()> {
+        let mut buf: [u8; 2] = [0; 2];
+        self.read_bytes(0x68, TEMP_OUT_H, &mut buf).unwrap();
+        let raw_temp = self.read_word_2c(&buf[0..2]) as f32;
+
+        // According to revision 4.2
+        Ok((raw_temp / TEMP_SENSITIVITY) + TEMP_OFFSET)
+    }
+
+    pub fn get_gyro(&self) -> Result<Vector3<f32>, ()> {
+        let mut gyro = self.read_rot(GYRO_REGX_H)?;
+
+        gyro *= PI_180 / self.gyro_sensitivity;
+
+        Ok(gyro)
+    }
+
+
+}
+
+
+#[cfg(test)]
+mod tests {
+}
